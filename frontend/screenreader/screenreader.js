b2ed3329cecf441d3c7974dcceaa8d2650202620
avonderg <77714730+avonderg@users.noreply.github.com>
2022-03-15 21:58:05 -0400

    feat: baseline functionality

--- frontend/screenreader/screenreader.js
+++ frontend/screenreader/screenreader.js
@@ -1,48 +1,47 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 // Look up the Web Speech API (https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis)
 // Initialize this variable when the window first loads
-let VOICE_SYNTH: SpeechSynthesis;
-
+let VOICE_SYNTH;
 // The current speaking rate of the screen reader
-let VOICE_RATE: number = 1;
-
+let VOICE_RATE = 1;
 // Stores elements and their handler functions
 // Think of an appropriate data structure to do this
 // Assign this variable in mapPage()
-let ELEMENT_HANDLERS: elementHandler[];
-
+let ELEMENT_HANDLERS;
 // Indicates the current element that the user is on
 // You can decide the type of this variable
-let current: string; // corresponds to ID of the element
-let prev: elementHandler;
-let PAUSED: boolean = false;
-
-
+let current; // corresponds to ID of the element
+let prev;
+let PAUSED = false;
 /**
  * Class for creating an element handler data structure, allowing HTML elements to be mapped to their handler
  * functions
  */
 class elementHandler {
-    elt: Element;
-    handler: Function
-
-    constructor(elt: Element, handler: Function) {
+    constructor(elt, handler) {
         this.elt = elt;
         this.handler = handler;
     }
 }
-
 /**
  * Speaks out text.
  * @param text the text to speak
  */
-function speak(text: string): void {
+function speak(text) {
     if (VOICE_SYNTH) {
         // initialize a speech request using SpeechSynthesisUtterance
         var utterance = new SpeechSynthesisUtterance(text);
         utterance.rate = VOICE_RATE;
-
         VOICE_SYNTH.speak(utterance);
-
         // check if it is speaking
         //
         // utterance.onstart = async function () {
@@ -53,21 +52,18 @@ function speak(text: string): void {
         //     });
         //     return;
         // }
-
         // onkeydown = async function (event : KeyboardEvent) {
         //     globalKeystrokes(event);
         // }
-
-
         // calls async func and waits for onend - if called several times
-        utterance.onstart = async function () { // pauses
-            await utterance.onend;
-            return;
-        }
+        utterance.onstart = function () {
+            return __awaiter(this, void 0, void 0, function* () {
+                yield utterance.onend;
+                return;
+            });
+        };
     }
 }
-
-
 window.onload = () => {
     generateHandlers();
     document.body.innerHTML = `
@@ -78,38 +74,32 @@ window.onload = () => {
             <button onclick="changeVoiceRate(0.9);">Slow Down [Left Arrow]</button>
         </div>
     ` + document.body.innerHTML;
-
     VOICE_SYNTH = window.speechSynthesis;
-
     document.addEventListener("keydown", globalKeystrokes);
-}
-
+};
 /**
  * Gets all the elements by their tagnames, sets their ID's, and inserts each element into the global
  * ELEMENT_HANDLERS array along with their handler functions
  */
-function generateHandlers(): void {
+function generateHandlers() {
     const collection = document.getElementsByTagName("*");
-
     let count = 0;
-    for (let elt of collection as any) {
-        elt.setAttribute('id',count.toString()); // sets unique numerical ID
-        count+=1;
+    for (let elt of collection) {
+        elt.setAttribute('id', count.toString()); // sets unique numerical ID
+        count += 1;
     }
-
-    ELEMENT_HANDLERS = Array<elementHandler>();
-    for (let elt of collection as any) {
-       ELEMENT_HANDLERS.push(new elementHandler(elt, handlers(elt)));
+    ELEMENT_HANDLERS = Array();
+    for (let elt of collection) {
+        ELEMENT_HANDLERS.push(new elementHandler(elt, handlers(elt)));
     }
 }
-
 /**
  * Function highlighting input HTML elements, to help partially-blind users recognize which section
  * is currently being read by the screen reader
  * @param text - text to highlight
  * @param elt - current element
  */
-function highlight(text: string, elt: Element): void{
+function highlight(text, elt) {
     // var inputText = elt.innerHTML
     //
     // var index = inputText.indexOf(text);
@@ -117,127 +107,125 @@ function highlight(text: string, elt: Element): void{
     //     inputText = inputText.substring(0, index) + "<span class='highlight'>" + inputText.substring(index, index + text.length) + "</span>" + inputText.substring(index + text.length);
     //     elt.innerHTML = inputText;
     // }
-    var element = <HTMLElement> elt;
+    var element = elt;
     element.style.backgroundColor = '2px solid yellow';
     return;
 }
-
 /**
  * Helper function generating handler functions for each of the different HTML elements being handled
  * @param elt
  */
-function handlers(elt: Element): Function {
+function handlers(elt) {
     if (elt.tagName === "TITLE") { // metadata
-        return function() {
+        return function () {
             speak("Title");
             highlight(elt.innerHTML, elt); // highlight element
             speak(elt.textContent || "");
-        }
+        };
     }
     else if (elt.tagName === "H1" || elt.tagName === "H2" || elt.tagName === "H3"
         || elt.tagName === "H4" || elt.tagName === "H5" || elt.tagName === "H6" || elt.tagName === "P") { //text
-        return function() {
+        return function () {
             if (elt.tagName != "P") {
                 speak("Header");
             }
             highlight(elt.innerHTML, elt); // highlight element
             speak(elt.textContent || "");
-        }
+        };
     }
     else if (elt.tagName === "IMG") { // graphics
-        var imageElt = <HTMLImageElement> elt;
-        return function() {
+        var imageElt = elt;
+        return function () {
             speak("Graphic:");
             if (imageElt.alt != null) { // if there is a caption
                 highlight(imageElt.alt, imageElt);
                 speak(imageElt.alt);
             }
-        }
+        };
     }
     else if (elt.tagName === "A") { // interactive
-        return function() {
+        return function () {
             speak("URL:");
             highlight(elt.innerHTML, elt);
             speak(elt.textContent || "");
-            }
-        }
+        };
+    }
     else if (elt.tagName === "LABEL") { // interactive : reads out label for an input form
-        return function() {
+        return function () {
             highlight(elt.innerHTML, elt);
             speak(elt.textContent || "");
-        }
+        };
     }
     else if (elt.tagName === "INPUT") { // interactive
-        return function() {
-            speak("Input:")
+        return function () {
+            speak("Input:");
             highlight(elt.innerHTML, elt);
             speak(elt.textContent || "");
-        }
+        };
     }
     else if (elt.tagName === "BUTTON") { // interactive
-        return function() {
-            speak("Button:")
+        return function () {
+            speak("Button:");
             highlight(elt.innerHTML, elt);
             speak(elt.textContent || "");
-        }
+        };
     }
     else if (elt.tagName === "TABLE") { // tables
-        var tableElt = <HTMLTableElement> elt;
-        return function() {
+        var tableElt = elt;
+        return function () {
             speak("Table:");
             highlight(elt.innerHTML, elt); // TODO: fix how tables are highlighted, and non-text objects
-        }
+        };
     }
     else if (elt.tagName === "CAPTION") { // tables
-        return function() {
+        return function () {
             speak("Caption:");
             highlight(elt.innerHTML, elt);
             speak(elt.textContent || "");
-        }
+        };
     }
     else if (elt.tagName === "TD") { // tables
-        return function() {
+        return function () {
             highlight(elt.innerHTML, elt);
             // speak(elt.textContent || "");
-        }
+        };
     }
     else if (elt.tagName === "TFOOT") { // tables
-        return function() {
+        return function () {
             speak("Summarizing column:"); // TODO: fix how it is announced
             highlight(elt.innerHTML, elt);
             speak(elt.textContent || "");
-        }
+        };
     }
     else if (elt.tagName === "TH") { // tables
-        return function() {
+        return function () {
             speak("Header:");
             highlight(elt.innerHTML, elt);
             speak(elt.textContent || "");
-        }
+        };
     }
     else if (elt.tagName === "TR") { // tables
-        return function() {
+        return function () {
             speak("Row:");
             highlight(elt.innerHTML, elt);
             // speak(elt.textContent || "");
-        }
+        };
     }
-    return function(){};
+    return function () { };
 }
-
 /**
  * Changes the speaking rate of the screen reader.
  * @param factor multiplier on the speaking rate
  */
-function changeVoiceRate(factor: number): void {
-    VOICE_RATE *= factor
+function changeVoiceRate(factor) {
+    VOICE_RATE *= factor;
     if (VOICE_RATE > 4) {
         VOICE_RATE = 4;
-    } else if (VOICE_RATE < 0.25){
+    }
+    else if (VOICE_RATE < 0.25) {
         VOICE_RATE = 0.25;
     }
 }
-
 /**
  * Moves to the next HTML element in the DOM.
  */
@@ -250,7 +238,6 @@ function next() {
         next();
     }
 }
-
 /**
  * Moves to the previous HTML element in the DOM.
  */
@@ -260,7 +247,6 @@ function previous() {
     prev.handler();
     next();
 }
-
 /**
  * Starts reading the page continuously.
  */
@@ -278,38 +264,37 @@ function start() {
         }
     }
 }
-
 /**
  * Pauses the reading of the page.
  */
- function pause() {
-     PAUSED = true;
-     VOICE_SYNTH.pause();
+function pause() {
+    PAUSED = true;
+    VOICE_SYNTH.pause();
 }
-
- /**
- * Resumes the reading of the page.
- */
-  function resume() {
-      PAUSED = false;
-      VOICE_SYNTH.resume();
- }
-
+/**
+* Resumes the reading of the page.
+*/
+function resume() {
+    PAUSED = false;
+    VOICE_SYNTH.resume();
+}
 /**
  * Listens for keydown events.
  * @param event keydown event
  */
-function globalKeystrokes(event: KeyboardEvent): void {
+function globalKeystrokes(event) {
     // can change and add key mappings as needed
     if (event.key === " ") {
         event.preventDefault();
         //TODO: start reading the entire page
         PAUSED = false;
         start();
-    } else if (event.key === "ArrowRight") {
+    }
+    else if (event.key === "ArrowRight") {
         event.preventDefault();
         changeVoiceRate(1.1);
-    } else if (event.key === "ArrowLeft") {
+    }
+    else if (event.key === "ArrowLeft") {
         event.preventDefault();
         changeVoiceRate(0.9);
     }
