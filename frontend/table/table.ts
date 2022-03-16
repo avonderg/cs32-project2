4b9472791420185c20f34ab8e47b42158f01fdd7
suraj_anand@brown.edu <surajK610@gmail.com>
2022-03-16 00:44:19 -0400

    add javadoc comments

--- frontend/table/table.ts
+++ frontend/table/table.ts
@@ -20,18 +20,19 @@ let rowCount : number = 0;
 
 fetch("http://localhost:4567/tableNames").then((res : Response) => res.json()).then((tableNames: string[]) => updateDropdown(tableNames))
 
+// updates dropdown menu as soon as table names are loaded from backend
 function updateDropdown(tableNames : string[]) : void { 
     dropdown.innerHTML = "";
     dropdown.innerHTML += "<select name=\"tables\" id=\"tableNames\"></select>"; 
     
     let tableNamesElement : HTMLSelectElement = document.getElementById("tableNames") as HTMLSelectElement; 
     tableNames.forEach((table : string) => tableNamesElement.innerHTML += `<option value=\"${table}\">${table}</option>`);
-    console.log(tableNames);
-
+    // added all the table names to the drop down menu
 }
 
 loadButton.addEventListener("click", load)
 
+// on load click, gets the table specified by dropdown menu from backend and then updates table html
 async function load() : Promise<void> {
     let tableNames : HTMLSelectElement = document.getElementById("tableNames") as HTMLSelectElement;
 
@@ -47,24 +48,28 @@ async function load() : Promise<void> {
         "Access-Control-Allow-Origin":"*"
         }
     });
+    // fetches the table data for the loaded sql database and table specified by dropdown from backend
 
     let tableData : Table = await res.json();
-    console.log(tableData);
+    // parses data to table 
 
     updateTable(tableData);
 }
 
+// updates the table specified by thte dropdown menu when load is clicked
 function updateTable(tableData : Table) : void{
     table.innerHTML = "";
     let headerRow : HTMLTableRowElement = table.insertRow()
 
     let headerMap : Map<number, string>  = insertHeaders(tableData.headers, headerRow)
+    // returns map from column index to header so that can insert row data at appropriate positions
+
     console.log(headerMap)
     console.log(tableData.rows)
     insertRows(table, tableData.rows, headerMap)
-    console.log("REACHED HERE")
 }
 
+// generates the html to insert the header row into the table
 function insertHeaders(headers : string[], headerRow : HTMLTableRowElement) : Map<number, string> {
     let map : Map<number, string>  = new Map<number, string>();
     
@@ -75,10 +80,11 @@ function insertHeaders(headers : string[], headerRow : HTMLTableRowElement) : Ma
     }
 
     return map;
+    // returns map from column index to header 
 }
 
+// generates the html to insert rows into the table
 function insertRows(table : HTMLTableElement, rows :  Record<string, string>[], headerMap :  Map<number, string>) {
-
     for (let i = 0; i < rows.length; i++) { 
         let currRow : HTMLTableRowElement = table.insertRow()
         for (let entry of Array.from(headerMap.entries())) { 
@@ -87,6 +93,7 @@ function insertRows(table : HTMLTableElement, rows :  Record<string, string>[], 
 
             let currCell : HTMLTableCellElement = currRow.insertCell(key)
             let currRecord : Record<string, string> = rows[i]
+            // gets the value for the specified column header (in this row)
 
             console.log(currRecord)
             console.log(key)
@@ -94,6 +101,7 @@ function insertRows(table : HTMLTableElement, rows :  Record<string, string>[], 
 
             let cellText = document.createTextNode(currRecord[value]);
             currCell.appendChild(cellText)
+            // puts the value intot the cell
         } 
     }
 }
