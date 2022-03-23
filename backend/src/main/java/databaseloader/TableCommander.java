4cd7d826c4ac33d17c566177192563e690948451
neil2001 <52434451+neil2001@users.noreply.github.com>
2022-03-23 00:20:19 -0400

    wrote comments and documentation

--- backend/src/main/java/databaseloader/TableCommander.java
+++ backend/src/main/java/databaseloader/TableCommander.java
@@ -1,12 +1,6 @@
 package databaseloader;
 
-
-import database.DatabaseCommander;
 import edu.brown.cs.student.main.CommandAcceptor;
-
-import java.io.FileNotFoundException;
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -14,7 +8,6 @@ import java.sql.SQLException;
  * db loading as well as testing.
  */
 public class TableCommander implements CommandAcceptor {
-  private final Connection conn = null;
   private static TableLoader db = null;
 
   /**
@@ -24,44 +17,32 @@ public class TableCommander implements CommandAcceptor {
   public static TableLoader getDb() {
     return db;
   }
-  public static void setDb(TableLoader t) {
-    db = t;
-  }
 
   /**
    * function that handles commands associated with the database.
    * @param input from cmd line.
    */
   public void handleCommand(String[] input) {
-    switch (input[0]) {
-      case "load_db":
-        if (input.length != 2) {
-          System.out.println("ERROR: Input must be of length two");
-          return;
-        }
-        try {
-          db = new TableLoader(input[1]);
-        } catch (SQLException e) {
-          System.out.println("ERROR: Could not connect to database");
-        } catch (ClassNotFoundException e) {
-          System.out.println("ERROR: Class not found??");
-        } catch (IllegalArgumentException e) {
-          System.out.println(e.getMessage());
-        }
+    if ("load_db".equals(input[0])) {
+      if (input.length != 2) {
+        System.out.println("ERROR: Input must be of length two");
+        return;
+      }
+      try {
+        db = new TableLoader(input[1]);
+      } catch (SQLException e) {
+        System.out.println("ERROR: Could not connect to database");
+      } catch (ClassNotFoundException e) {
+        System.out.println("ERROR: Class not found??");
+      } catch (IllegalArgumentException e) {
+        System.out.println(e.getMessage());
+      }
 
-        if (db != null) {
-          System.out.println("Loaded database from file " + input[1]);
-        }
-        break;
+      if (db != null) {
+        System.out.println("Loaded database from file " + input[1]);
+      }
+    } else {
+      System.out.println("ERROR: Invalid command");
     }
-  }
-
-  /**
-   * Method to print the result set.
-   * @param results a ResultSet from a query.
-   * @throws SQLException if the results do not have metadata.
-   */
-  public void printResults(ResultSet results) throws SQLException {
-    DatabaseCommander.printResults(results);
   }
 }
